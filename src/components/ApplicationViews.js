c0d677f11736ccf83ff2ad7cd04011c95622d0b5
Chase Sullins <jcsullins222@yahoo.com>
2020-02-21 13:32:01 -0600

    completed the Locations card for Chapter 1 exercise

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,37 @@
+import { Route } from "react-router-dom";
+import React from "react";
+import Home from "./home/Home";
+import AnimalCard from "./animal/AnimalCard";
+import LocationCard from "./location/LocationCard"
+//only include these once they are built - previous practice exercise
+// import LocationCard from "./location/LocationCard";
+// import EmployeeCard from "./employee/EmployeeCard";
+// import OwnerCard from "./owner/OwnerCard";
+
+const ApplicationViews = () => {
+  return (
+    <React.Fragment>
+      <Route
+        exact
+        path="/"
+        render={props => {
+          return <Home />;
+        }}
+      />
+      <Route
+        path="/animals"
+        render={props => {
+          return <AnimalCard />;
+        }}
+      />
+      <Route
+        path="/locations"
+        render={props => {
+          return <LocationCard />;
+        }}
+      />
+    </React.Fragment>
+  );
+};
+
+export default ApplicationViews;
---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const EmployeeCard = () => {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h1>Employees:</h1>
+        <section>
+            <ul>
+                <li>Chase</li>
+                <li>Chance</li>
+                <li>Chase</li>
+                <li>Chase</li>
+            </ul>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeCard;
